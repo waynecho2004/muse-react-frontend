8a886033a62f500539a10334ec8cdc343ca4938e
Wayne Cho <waynecho2004@gmail.com>
2020-12-08 16:31:51 -0500

    SongContainer - create addSong(song) and pass to CreateSongForm

--- src/SongContainer.js
+++ src/SongContainer.js
@@ -5,14 +5,14 @@ import CreateSongForm from './CreateSongForm'
 import { Grid } from 'semantic-ui-react';
 
 class SongContainer extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
       songs: []
     }
   }
-  componentDidMount(){
+  componentDidMount() {
     this.getSongs();
   }
   getSongs = async () => {
@@ -28,24 +28,52 @@ class SongContainer extends Component {
       console.log(err);
     }
   };
-  
-  addSong = (e, song) => {  
+
+  // It's a good practice to make it async to enhance performance
+  addSong = async (e, song) => { 
+    e.preventDefault();
     console.log(song);
+
+    try {
+      // The createdSongResponse variable will store the response from the Flask API
+      // source: github.com/axios/axios
+      const createdSongResponse = await axios({
+        method: 'POST',
+        url: process.env.REACT_APP_FLASK_API_URL + '/api/v1/songs/',
+        data: song,  // this is the song json object
+        headers: {   // make sure the content-type is 'application/json'
+          'Content-Type': 'application/json',
+        },
+      });
+
+      // we are emptying all the songs that are living in state into a new array,
+      // and then adding the song we just created to the end of it
+      // the new song which is called parsedResponse.data
+      console.log(createdSongResponse.data.data, ' this is response');
+
+      // Following is more efficient way adding new song:
+      //    adding to the state if successfully, instead of calling api to reload
+      this.setState({
+        songs: [...this.state.songs, createdSongResponse.data.data],
+      });
+    } catch (err) {
+      console.log('error', err);
+    }
   };
 
-  render(){
+  render() {
     return (
-        <Grid columns={2} divided textAlign='center' style={{ height: '100%' }} verticalAlign='top' stackable>
-          <Grid.Row>
-            <Grid.Column>
-              <SongList songs={this.state.songs}/>
-            </Grid.Column>
-            <Grid.Column>
-            <CreateSongForm addDog={this.addSong}/>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      )
+      <Grid columns={2} divided textAlign='center' style={{ height: '100%' }} verticalAlign='top' stackable>
+        <Grid.Row>
+          <Grid.Column>
+            <SongList songs={this.state.songs} />
+          </Grid.Column>
+          <Grid.Column>
+            <CreateSongForm addSong={this.addSong} />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    )
   }
 }
 
